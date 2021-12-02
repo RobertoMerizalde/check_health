93bbd7783c153146669746d8ebbcb901e56f37a0
RobertMerix <romer_96p@outlook.com>
2021-12-02 01:04:06 -0600

    Add a better version of Heath Checks

---
+++ health_check.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import os
+import psutil
+import shutil
+import sys
+import socket
+
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabyes_free = du.free / 2**30
+    print("{} Free Disk Storage.".format(gigabyes_free))
+    if gigabyes_free < min_gb or percent_free < min_percent:
+        return True
+    return False
+
+
+def check_root_full():
+    """Returns True if the root partition is full, False otherwise"""
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+
+def check_cpu_constrained():
+    """Returns True if the cpu is having too much usage, False otherwise."""
+    cpu_constrained = psutil.cpu_percent(1)
+    print("Current CPU constrained: {}.".format(cpu_constrained))
+    return cpu_constrained > 75
+
+
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostname("www.google.com")
+        return False
+    except:
+        return True
+
+
+def main():
+    checks = [
+        (check_reboot, "Pending Reboot."),
+        (check_root_full, "Root partition full."),
+        (check_cpu_constrained, "CPU load too high."),
+    ]
+
+    everything_ok = True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok = False
+
+    if not everything_ok:
+        sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
+
+
+main()
+
